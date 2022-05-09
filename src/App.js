d70f3590d7bc2b9ed7f7a61c6a3c9d9bdb7ed2f5
dariancmagee <dariancmagee@gmail.com>
2022-05-08 21:35:26 -0400

    updated server side

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import PlannerList from './components/PlannerList';
 
 function App() {
+
+  const [backendData, setBackendDate] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackenData(data)
+      }
+    )
+  })
   return (
     <div className="planner-app">
       <PlannerList />
--- src/components/PlannerList.js
+++ src/components/PlannerList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PlannerForm from './PlannerForm';
+import Planner from './Planner';
 
 function PlannerList() {
     const [plans, setPlans] = useState([]);
@@ -11,14 +12,40 @@ function PlannerList() {
 
         const newPlans = [plan, ...plans]
 
-        setPlans(newPlans)
+        setPlans(newPlans);
         
+    };
+
+
+    const updatePlan = (planId, newValue) => {
+      if(!newValue.text || /^\s*$/.test(newValue.text)) {
+        return;
+      }
+
+      setPlans(prev => prev.map(item => (item.id === planId ? newValue: item)))
+    }
+
+    const removePlan = id => {
+      const removeArr = [...plans].filter(plan => plan.id !== id)
+
+      setPlans(removeArr);
+    };
+
+
+    const completePlan = id => {
+      let updatedPlans = plans.map(plan => {
+        if (plan.id === id) {
+          plan.isComplete = !plan.isComplete;
+        }
+        return plan;
+      });
     };
 
   return (
     <div>
         <h1>Plans for today!</h1>
         <PlannerForm onSubmit={addPlan} />
+        <Planner plans={plans} completePlan={completePlan} removePlan={removePlan} updatePlan={updatePlan} />
     </div>
   )
 }
